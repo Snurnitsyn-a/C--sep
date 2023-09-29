af0f04dee6a31d437603abff85a33f1046231b5e
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-09-30 00:13:27 +0300

    Task 10

---
+++ Home Work/HW_sem2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, которая 
+принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Input a three digit number: ");
+int a = int.Parse(Console.ReadLine());
+int result = a/10%10;
+Console.Write(result);
+
