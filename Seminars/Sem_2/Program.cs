88453cc4aea65e8b00945f32685c043e878d805c
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-09-29 23:17:21 +0300

    First task sem 2

---
+++ Seminars/Sem_2/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Задача 11 
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа*/
+using System;
+using static System.Console;
+int num = new Random().Next(100, 1000);
+
+int result = num / 100 * 10 + num % 10;
+Write($"{num} -> {result}");
+
