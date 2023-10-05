5712668b560ab34a10d5b3f0979edbc109239033
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-10-05 23:38:47 +0300

    Seminar 3, task 1

---
+++ Seminars/Sem_3/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 1 
+Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+456 -> 3
+78 -> 2
+765434 -> 6
+*/
+
+Console.WriteLine("Input number please: ");
+int x = int.Parse(Console.ReadLine());
+int digits = GetDigits2(x);
+Console.WriteLine($"The number of digits is {digits}");
+
+
+int GetDigits(int number) // Method 1
+{
+    string digitsString = number.ToString();
+    int count = digitsString.Length;
+    return count;
+}
+
+int GetDigits2(int number) // Method 2
+{
+    int count = 0;
+    while (number != 0)
+    {
+        number /= 10;
+        count ++;
+    }
+    return count;
+}
---
+++ case/Program.cs
@@ -0,0 +1,1 @@
+﻿int[] array = new Console.Random()
